a78723604c3b433d5c08ece96c8d45d21ef4dd9a
David Ringgenberg <david.ringgenberg@ringgi.net>
2020-06-17 23:40:39 +0200

    feat: Setup MovieFormPage
    Setup the Movie form for both a new Movie and to edit an existing movie

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { ModalController } from '@ionic/angular';
 import { MoviesService, Movie } from '../services/movies.service';
+import { MovieFormPage } from './movie-form/movie-form.page';
 
 @Component({
   selector: 'app-tab1',
@@ -10,15 +11,30 @@ import { MoviesService, Movie } from '../services/movies.service';
 })
 export class Tab1Page implements OnInit {
   Object = Object;
+  loginURL: string;
 
   constructor(
     private auth: AuthService,
-    private modalCtrl: ModalController,
+    private modalController: ModalController,
     public movies: MoviesService
-  ) {}
+  ) {
+    this.loginURL = auth.build_login_link('/tabs/movies');
+  }
 
   ngOnInit() {
     this.movies.getMovies();
+  }
+
+  async presentModal(movie: Movie = null) {
+    const modal = await this.modalController.create({
+      component: MovieFormPage,
+      cssClass: 'movie-form-class',
+      componentProps: {
+        'movie': movie,
+        'isNew': !movie
+      }
+    });
+    return await modal.present();
   }
 
 }
