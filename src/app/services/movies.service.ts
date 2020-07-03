cecb5b78947af7a8952174fe2f58dd45f6e509c1
David Ringgenberg <david.ringgenberg@ringgi.net>
2020-07-03 16:38:31 +0200

    feat: Post, Patch and Delete Movie
    Updated getMovie() to show a toast if an error happens.
    Add postMovie() will send a POST request to the API and based on the response it will show the respected toast
    Add patchMovie() will send a PATCH request to the API and based on the response it will show the respected toast
    Add deleteMovie() will send a DELETE request to the API and based on the response it will show the respected toast
    Add presentToast() to show a new Toast when called

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -3,13 +3,17 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { AuthService } from './auth.service';
 import { environment } from 'src/environments/environment';
+import { ToastController } from '@ionic/angular';
 import { Actor } from './actors.service';
 
 export interface Movie {
   id: number;
   title: string;
   release_date: string;
-  cast: {[key: number]: Actor};
+  cast: Array<{
+    id: number,
+    name: string
+  }>;
 }
 
 @Injectable({
@@ -20,7 +24,7 @@ export class MoviesService {
   public active_movie: Movie;
   public items: {[key: number]: Movie} = {};
 
-  constructor(private auth: AuthService, private http: HttpClient) { }
+  constructor(private auth: AuthService, private http: HttpClient, public toastController: ToastController) { }
 
   getHeaders() {
     const header = {
@@ -34,9 +38,65 @@ export class MoviesService {
     if (this.auth.can('get: movies')) {
       this.http.get(this.url + '/movies', this.getHeaders())
       .subscribe((res: any) => {
-        this.moviesToItems(res.movies);
-        console.log(res);
+        if (res.success) {
+          this.moviesToItems(res.movies);
+        } 
+        else if (!res.success) {
+          this.presentToast("danger", "Error: " + res.error + ": " + res.message)
+        }
       });
+    }
+  }
+
+  postMovie(title: string, release_date: string) {
+    try {
+      if(this.auth.can('post: movies')) {
+        this.http.post(this.url + '/movies', {
+          "title": title,
+          "release_date": release_date
+        }, this.getHeaders())
+        .subscribe((res: any) => {
+          if (res.success) {
+            this.presentToast("primary", "Movie added to database")
+          } 
+          else if (!res.success) {
+            this.presentToast("danger", "Error: " + res.error + ": " + res.message)
+          }
+        })
+      }
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  patchMovie(editedMovie: Movie) {
+    if(this.auth.can('patch: movies')) {
+      this.http.patch(this.url + '/movies/' + editedMovie.id, {
+        "title": editedMovie.title,
+        "release_date": editedMovie.release_date,
+        "cast": editedMovie.cast
+      }, this.getHeaders())
+      .subscribe((res: any) => {
+        if (res.success) {
+          this.presentToast("primary", "Movie updated in database")
+        } 
+        else if (!res.success) {
+          this.presentToast("danger", "Error: " + res.error + ": " + res.message)
+        }
+      })
+    }
+  }
+
+  deleteMovie(movieId: number) {
+    if(this.auth.can('delete: movies')) {
+      this.http.delete(this.url + '/movies/' + movieId, this.getHeaders()).subscribe((res: any) => {
+        if (res.success) {
+          this.presentToast("primary", "Movie deleted from database")
+        } 
+        else if (!res.success) {
+          this.presentToast("danger", "Error: " + res.error + ": " + res.message)
+        }
+      })
     }
   }
 
@@ -44,6 +104,16 @@ export class MoviesService {
     for (const movie of movies) {
       this.items[movie.id] = movie;
     }
+  }
+
+
+  async presentToast(color: string, message: string) {
+    const toast = await this.toastController.create({
+      color: color,
+      message: message,
+      duration: 2000
+    });
+    toast.present();
   }
   
 }
